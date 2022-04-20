ada5d9f8eb089051eee8d924b7afe1e140163850
morysol <cepera.cMupHoB@gmail.com>
2022-04-20 12:54:38 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,12 +2,13 @@ const _ = require('lodash');
 
 const feedbackForm = document.querySelector('.feedback-form');
 
-// submit
+// submiting
 
 feedbackForm.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
+
   const {
     elements: { email, message },
   } = event.currentTarget;
@@ -16,43 +17,56 @@ function handleSubmit(event) {
     return console.log('Please fill in all the fields!');
   }
 
-  console.log(`email: ${email.value}, message: ${message.value}`);
+  console.log(` ------> submited email: ${email.value}, message: ${message.value}`);
+
+  loggingEmailMessage('email', '');
+  loggingEmailMessage('message', '');
+
+  document.querySelector('textarea[name=message]').textContent = '';
+
   event.currentTarget.reset();
 }
 
 // logging
 
-let storageInProgress = {
-  email: '',
-  message: '',
-};
-
 const localStorageLable = 'feedback-form-state';
 const localStorageEmailMessage = localStorage.getItem(localStorageLable);
 
-// if (localStorageEmailMessage.email && localStorageEmailMessage.message) {
-// if (
-//   localStorageEmailMessage.email !== undefined &&
-//   localStorageEmailMessage.message !== undefined
-// ) {
+// console.log(localStorage.getItem('zzxxcc'));
+// testObj = JSON.parse(localStorage.getItem('zzxxcc'));
+// console.log(localStorage.getItem('testObj'));
+// if (testObj === null) {
+//   alert(null);
 // }
 
-storageInProgress = JSON.parse(localStorageEmailMessage);
+// if (localStorageEmailMessage.email !== undefined) {
+//   console.log(localStorageEmailMessage.email);
+// }
+// if (localStorageEmailMessage.message !== undefined) {
+//   console.log(localStorageEmailMessage.message);
+// }
+
+let storageInProgress = JSON.parse(localStorageEmailMessage);
+
+if (storageInProgress === null) {
+  storageInProgress = {
+    email: '',
+    message: '',
+  };
+}
+
 console.log(
-  storageInProgress.email,
-  localStorageEmailMessage,
+  // storageInProgress.email,
+  // localStorageEmailMessage,
   JSON.parse(localStorageEmailMessage),
 );
-
-// document.querySelector('input[name=email]').textContent = storageInProgress.email;
-// document.querySelector('textarea[name=message]').textContent = storageInProgress.message;
 
 document.querySelector('input[name=email]').value = storageInProgress.email;
 document.querySelector('textarea[name=message]').textContent = storageInProgress.message;
 
-feedbackForm.addEventListener('input', _.throttle(getInputedString, 500));
+feedbackForm.addEventListener('input', _.throttle(logInputedString, 500));
 
-function getInputedString(e) {
+function logInputedString(e) {
   //   console.log(e, e.target.value, e.target.type, e.target.hasAttribute('autofocus'));
   const name = e.target.name;
   const value = e.target.value;
@@ -71,8 +85,10 @@ function getInputedString(e) {
 }
 
 function loggingEmailMessage(name, value) {
-  console.log('logging', name, value);
-  console.log((storageInProgress[name] = value));
+  // console.log('logging', name, value);
+  // console.log(storageInProgress);
+
+  storageInProgress[name] = value;
 
   localStorage.setItem(localStorageLable, JSON.stringify(storageInProgress));
 }
